81c423cfffa15fc15e7a705a510747bf300e220b
robertogriel <roberto@griel.com.br>
2021-09-24 21:02:36 -0300

    Server Side Renderer Fix

---
+++ components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Footer = styled.footer`
+    height: 95px;
+    width: 100vw;
+    background-color: #181818;
+    display: flex;
+    justify-contents: center;
+    align-items: center; 
+`;
+
+export default Footer;
+
